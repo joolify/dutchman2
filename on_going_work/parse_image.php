402f8ed24b8d6e2124d111ef987dd48b3a15174c
joelwe@gmail.com <hem@nl119-196-62.student.uu.se>
2015-02-05 13:05:32 +0100

    parse_image

---
+++ on_going_work/parse_image.php
@@ -0,0 +1,14 @@
+<?php
+include('simple_html_dom.php');
+$html = file_get_html('http://www.systembolaget.se/Sok-dryck/Dryck/?artikelId=335527&varuNr=6697');
+
+
+// find all div tags with id=gbar
+foreach($html->find('div#ctl00_FullRegion_LeftCenterRegion_ProductImageDiv') as $div);
+
+foreach($div->find('a') as $img);
+
+
+
+echo '<img src="http://www.systembolaget.se/Sok-dryck/Dryck' . $img->href . '">';
+?>
