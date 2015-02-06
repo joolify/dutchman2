3e44b10c2d6ee11cf05320a8948bd6198f89e7ef
joelwe@gmail.com <hem@Joels-MacBook-Pro.local>
2015-02-06 09:38:31 +0100

    merge fix

--- on_going_work/index.php
+++ on_going_work/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,17 +10,3 @@
     </form>
   </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Stormrider</title>
-  </head>
-  <body>
-    <form>
-      <h1>Welcome</h1>
-      <button formaction="login.php">VIP</button>
-    </form>
-  </body>
-</html>
->>>>>>> 8f6248e71c749e84b0e7d04c43b77bf8b86cff54
