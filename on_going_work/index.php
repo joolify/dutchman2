e5507e055370446034a8539981f16da212695103
Daniel Llatas Spiers <danllatas@gmail.com>
2015-02-09 17:29:04 +0100

    Changes on the CSS for the index

--- on_going_work/index.php
+++ on_going_work/index.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Stormrider</title>
-  </head>
-  <body>
-    <form>
-      <h1>Welcome</h1>
-      <button formaction="login.php">VIP</button>
-    </form>
-  </body>
+	<head>
+    	<meta charset="utf-8">
+    	<title>Stormrider</title>
+    	<link rel="stylesheet" href="assets/css/main.css">
+    	<link href='http://fonts.googleapis.com/css?family=Prociono' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="jumbotron">
+	    	<div class="container">
+	        	<h1>Stormrider Bar</h1>
+	        	<p>Welcome to the best Uppsala bar!</p>
+			</div>
+		</div>
+		<div class="container">
+			<form class="login-form" action="login.php">
+		        <div>
+		        	<input type="text" placeholder="Username">
+		        </div>
+		        <div>
+		        	<input type="password" placeholder="Password">
+		        </div>
+		        <button type="submit">Log in</button>
+		      </form>
+	    </div>  
+    </body>
 </html>
